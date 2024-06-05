3337a3b5a37a6b2b05c5374928c741d57f4c43d7
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-06-05 21:44:40 +0200

    put the units context in the root layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,46 +4,42 @@ import { Tabs } from 'expo-router';
 
 import TabBarIcon from '@/element/tab-bar-icon';
 
-import UnitsContextProvider from '@/context/units-context';
-
 import { Colors } from '@/constant/Colors';
 
 const TabLayout = () => {
   return (
-    <UnitsContextProvider>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors['light'].tint,
-          headerShown: false,
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors['light'].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Accueil',
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />,
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Accueil',
-            tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />,
-          }}
-        />
+      />
 
-        <Tabs.Screen
-          name="recipes"
-          options={{
-            title: 'Recettes',
-            tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'book' : 'book-outline'} color={color} />,
-          }}
-        />
+      <Tabs.Screen
+        name="recipes"
+        options={{
+          title: 'Recettes',
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'book' : 'book-outline'} color={color} />,
+        }}
+      />
 
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: 'Paramètres',
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon name={focused ? 'settings' : 'settings-outline'} color={color} />
-            ),
-          }}
-        />
-      </Tabs>
-    </UnitsContextProvider>
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Paramètres',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'settings' : 'settings-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,22 +5,25 @@ import AuthContextProvider from '@/context/auth-context';
 import CartContextProvider from '@/context/cart-context';
 import CategoriesContextProvider from '@/context/categories-context';
 import RecipesContextProvider from '@/context/recipes-context';
+import UnitsContextProvider from '@/context/units-context';
 
 const RootLayout = () => {
   return (
     <AuthContextProvider>
-      <CategoriesContextProvider>
-        <RecipesContextProvider>
-          <CartContextProvider>
-            <ThemeProvider value={DefaultTheme}>
-              <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="+not-found" />
-              </Stack>
-            </ThemeProvider>
-          </CartContextProvider>
-        </RecipesContextProvider>
-      </CategoriesContextProvider>
+      <UnitsContextProvider>
+        <CategoriesContextProvider>
+          <RecipesContextProvider>
+            <CartContextProvider>
+              <ThemeProvider value={DefaultTheme}>
+                <Stack>
+                  <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                  <Stack.Screen name="+not-found" />
+                </Stack>
+              </ThemeProvider>
+            </CartContextProvider>
+          </RecipesContextProvider>
+        </CategoriesContextProvider>
+      </UnitsContextProvider>
     </AuthContextProvider>
   );
 };
